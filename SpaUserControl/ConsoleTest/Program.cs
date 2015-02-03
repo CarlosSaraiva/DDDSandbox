23c45f1f77f5249cd463adb9a55b7880f9e90381
Carlos Saraiva <carlos.saraiva@msn.com>
2015-02-03 11:18:26 -0200

    ConsoleTest added

---
+++ SpaUserControl/ConsoleTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaUserControl.Domain.Models;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+            user.Id = 1;
+            user.Name = "Carlos";
+            user.Email = "carlos.saraiva@msn.com";
+            user.Password = "123456";
+
+            Console.WriteLine(user.Name);
+            Console.ReadKey();
+        }
+    }
+}
