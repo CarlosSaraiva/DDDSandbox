9be64c898de452328dbebff833910d3ed23d2d76
Carlos Saraiva <carlos.saraiva@msn.com>
2015-02-03 16:24:48 -0200

    UserSpaControl.Infrastructure created

---
+++ SpaUserControl/SpaUserControl.Infrastructure/Data/AppDataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using SpaUserControl.Domain.Models;
+using SpaUserControl.Infrastructure.Data.Map;
+
+namespace SpaUserControl.Infrastructure.Data
+{
+    public class AppDataContext:DbContext
+    {
+        public AppDataContext():base("AppConnectionString")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new UserMap());
+        }
+    }
+}
