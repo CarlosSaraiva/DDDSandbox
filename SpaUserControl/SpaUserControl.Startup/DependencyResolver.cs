da81ff5a17ede0a203b2cc0c38f67e90b1a08646
Carlos <carlos.saraiva@msn.com>
2015-02-05 14:40:17 -0200

    Startup domain created

---
+++ SpaUserControl/SpaUserControl.Startup/DependencyResolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using SpaUserControl.Domain.Contracts.Repositories;
+using SpaUserControl.Infrastructure.Data;
+using SpaUserControl.Infrastructure.Repositories;
+
+namespace SpaUserControl.Startup
+{
+    class DependencyResolver
+    {
+        public static void Resolve(UnityContainer container)
+        {
+            container.RegisterType<AppDataContext, AppDataContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserRepository, UserRepository>(new HierarchicalLifetimeManager());
+        }
+    }
+}
